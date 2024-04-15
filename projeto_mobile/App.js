1cb1e4a10717f395b80f5171b7d864c45342e31b
BuenoMVP <marcos_vbp@outlook.com>
2024-04-15 16:31:41 -0300

    Aplicando modo horizontal

--- projeto_mobile/App.js
+++ projeto_mobile/App.js
@@ -1,15 +1,32 @@
 //Aplicação
 //Imports
-import { View, Text, TextInput } from 'react-native'
+import { View, Text, TextInput, StyleSheet } from 'react-native'
 
 const App = () => {
     return(
-        <View>
-            <Text>Peso:</Text>
-            <TextInput></TextInput>
+        <View style={estilos.viewMae}>
+            <View>
+                <Text style={estilos.texto}>Satisfyng.you</Text>
+            </View>
         </View>
     ) 
 }
+
+const estilos = StyleSheet.create({
+    viewMae: {
+        backgroundColor: "#372775", 
+        width: "100%", 
+        height: "100%", 
+        justifyContent: "center", 
+        alignItems: "center", 
+        gap: 30
+    }, 
+    texto: {
+        color: "white", 
+        fontFamily: "AveriaLibre-Regular", 
+        fontSize: 30
+    }
+})
 
 //Exportação
 export default App
