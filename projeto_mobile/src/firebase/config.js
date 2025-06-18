f71752293a44bce92868c3ec7ae807a000b46536
Marcos Bueno <marcos_vbp@outlook.com>
2025-06-17 23:36:56 -0300

    feat: conexao com firebase

---
+++ projeto_mobile/src/firebase/config.js
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAJu4hsA5KGBxEQNq4TYEqIRYOOtry8Ru8",
+  authDomain: "prog-mobile-es47a.firebaseapp.com",
+  projectId: "prog-mobile-es47a",
+  storageBucket: "prog-mobile-es47a.firebasestorage.app",
+  messagingSenderId: "160711513967",
+  appId: "1:160711513967:web:02330c27e2d6a01bf03a9a",
+  measurementId: "G-YSF97729Z2"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth_module = getAuth(app);
+
+export { auth_module };
