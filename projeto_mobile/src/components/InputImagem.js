6ef2280bcbc0f4ac82e1f9dda9af8e8669a5ef0f
Leonardo Moreira <leonardo.moreira@wolven.com.br>
2025-06-28 13:19:27 -0300

    Adicionar seletor e redimensionador de imagens ao componente InputImagem

--- projeto_mobile/src/components/InputImagem.js
+++ projeto_mobile/src/components/InputImagem.js
@@ -1,37 +1,71 @@
-import { Pressable, StyleSheet, Text, View, Image } from "react-native";
-import { useState } from "react";
-import { Modal } from "react-native-paper";
+import React, {useState} from 'react';
+import {Image, Pressable, StyleSheet, Text, View} from 'react-native';
+import {launchImageLibrary} from 'react-native-image-picker';
+import ImageResizer from 'react-native-image-resizer';
 
-const InputImagem = (props) => {
+const InputImagem = props => {
+  const [imagem, setImagem] = useState(props?.image);
 
-    return (
-        <View style={{gap: 2}}>
-            <Text style={estilos.texto}>{props.label} </Text>
-            <Pressable style={estilos.input}  {...props}>
-                <Image style={estilos.imagem} source={{uri: props.image}}/>
-            </Pressable>
-        </View>
-        
-    )
-}
+  const pickImage = async () => {
+    const options = {
+      mediaType: 'photo',
+    };
+
+    launchImageLibrary(options, result => {
+      convertUriToBase64(getImageUri(result));
+    });
+  };
+
+  const getImageUri = result => {
+    return result?.assets[0]?.uri;
+  };
+
+  const convertUriToBase64 = async uri => {
+    const resizedImage = await ImageResizer.createResizedImage(
+      uri,
+      500,
+      500,
+      'JPEG',
+      100,
+    );
+
+    const imageUri = await fetch(resizedImage.uri);
+    const imageBlob = await imageUri.blob();
+
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setImagem(reader.result);
+    };
+    reader.readAsDataURL(imageBlob);
+  };
+
+  return (
+    <View style={{gap: 2}}>
+      <Text style={estilos.texto}>{props.label} </Text>
+      <Pressable style={estilos.input} {...props} onPress={pickImage}>
+        <Image style={estilos.imagem} source={{uri: imagem}} />
+      </Pressable>
+    </View>
+  );
+};
 
 const estilos = StyleSheet.create({
-    input: {
-        height: 70,
-        width: "50%",
-        backgroundColor: "white", 
-        justifyContent: "center", 
-        alignItems: "center"
-    },
-    texto: {
-        color: "white",
-        fontFamily: "AveriaLibre-Regular",
-        fontSize: 20
-    }, 
-    imagem: {
-        height: 60,
-        width: 60
-    }
-})
+  input: {
+    height: 70,
+    width: '50%',
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  texto: {
+    color: 'white',
+    fontFamily: 'AveriaLibre-Regular',
+    fontSize: 20,
+  },
+  imagem: {
+    height: 60,
+    width: 60,
+  },
+});
 
-export default InputImagem
+export default InputImagem;
